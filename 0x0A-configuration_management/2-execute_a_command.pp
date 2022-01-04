eed6e186d32e3e0cf63fdca29a3ff5d29125c544
Ele4327 <sebasgarzons88@gmail.com>
2022-01-03 23:34:35 -0500

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # Kill a process by running
 
 exec { 'killmenow':
-    command   => 'pkill -f ./killmenow',
     path      => 'usr/bin',
+    command   => 'pkill killmenow',
+    provider => 'shell',
 }
