7cfc3672631115e46f81551217211287748f59ae
Ele4327 <sebasgarzons88@gmail.com>
2022-01-03 23:58:55 -0500

    Install a package

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Kill a process by running
-
 exec { 'killmenow':
-    path      => '/usr/bin',
-    command   => 'pkill -f ./killmenow',
+  path    => '/usr/bin',
+  command => 'pkill killmenow',
 }
